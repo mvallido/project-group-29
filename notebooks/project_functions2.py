a1149a3eaa9362c5e23d1a5c6d187b907522e5c5
mvallido <vallido.michael@gmail.com>
2021-10-31 13:59:21 -0700

    Small edits

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -1,8 +1,11 @@
+import pandas as pd
+import numpy as np
+
 def load_and_process(url):
     df = (
         pd
         .read_csv(url)
-        .drop(columns=['Phase','Time Period','Subgroup','Time Period Start Date','Time Period Start Date','Time Period End Date','Low  CI','High CI','Quartile Range'])
+        .drop(columns=['Phase','Time Period','Subgroup','Time Period Start Date','Time Period Start Date','Time Period End Date','Low CI','High CI','Quartile Range'])
         .dropna()
         .rename(columns={"Value":"Percentage"})
     )
@@ -10,5 +13,8 @@ def load_and_process(url):
     df.drop(df[df["Indicator"] != "Symptoms of Depressive Disorder"].index, inplace=True)
     df.drop(df[df["Group"] != "By State"].index, inplace=True)
     df.drop(df[df["Time Period Label"] != "Sep 15 - Sep 27, 2021"].index, inplace=True)
-    
+
+
     return df.reset_index(drop=True)
+
+
